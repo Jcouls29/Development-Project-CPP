f15745fa81f48937dc948ad77f33783958d4f633
Greg Suing <greg.d.suing@saic.com>
2022-03-16 09:51:00 -0500

    submission for interview aptitude test

--- Development Project/DataAccess.Lib/recordSet.cpp
+++ Development Project/DataAccess.Lib/recordSet.cpp
@@ -4,7 +4,9 @@ std::string recordSet::getData(const int row, const std::string column) {
 
     // TODO: get data from row and column in record set
 
-    // get column info using SQLDescribeColW(_sqlStmtHandle, i, col_name, 40, &len, &type, &size, &scale,
+    // get column info using SQLDescribeCol
+
+    // throw on invalid row index or bad column name
 
 	return "TODO: recordSet::getData()";
 }
